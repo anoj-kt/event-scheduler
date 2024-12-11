5c15f0b11b204f3bcb952bfa282c1fcc3ea3dca8
anoj-kt <anojk28@gmail.com>
2024-12-11 17:00:44 +0100

    Added eventDetail fetching

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 const EventCard = ({ event }) => {
   const formattedDate = new Date(event.date).toLocaleDateString('en-GB', {
     day: '2-digit',
@@ -6,25 +8,27 @@ const EventCard = ({ event }) => {
   });
 
   return (
-    <div className='card bg-neutral w-full shadow-xl mb-4'>
-      <div className='card-body'>
-        <h2 className='card-title text-primary'>
-          {event.title}
-          <div className='badge badge-secondary'>{formattedDate}</div>
-        </h2>
-        <div className='flex gap-2 my-2'>
-          <svg
-            className='h-6 fill-primary'
-            xmlns='http://www.w3.org/2000/svg'
-            viewBox='0 0 384 512'
-          >
-            <path d='M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z' />
-          </svg>
-          <span>{event.location}</span>
+    <Link to={`event/${event.id}`}>
+      <div className='card bg-neutral w-full shadow-xl mb-4'>
+        <div className='card-body'>
+          <h2 className='card-title text-primary'>
+            {event.title}
+            <div className='badge badge-secondary'>{formattedDate}</div>
+          </h2>
+          <div className='flex gap-2 my-2'>
+            <svg
+              className='h-6 fill-primary'
+              xmlns='http://www.w3.org/2000/svg'
+              viewBox='0 0 384 512'
+            >
+              <path d='M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z' />
+            </svg>
+            <span>{event.location}</span>
+          </div>
+          <p className='truncate'>{event.description}</p>
         </div>
-        <p className='truncate'>{event.description}</p>
       </div>
-    </div>
+    </Link>
   );
 };
 
--- src/pages/EventDetails.jsx
+++ src/pages/EventDetails.jsx
@@ -1,8 +1,30 @@
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
 const EventDetails = () => {
+  const { eventID } = useParams();
+  const [eventDetails, setEventDetails] = useState({});
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/api/events/${eventID}`)
+      .then((res) => res.json())
+      .then((data) => setEventDetails(data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  const formattedDate = new Date(eventDetails.date).toLocaleDateString(
+    'en-GB',
+    {
+      day: '2-digit',
+      month: 'short',
+      year: '2-digit',
+    }
+  );
+
   return (
     <main className='max-w-screen-2xl px-4 my-4'>
-      <h1 className='text-3xl mb-4'>Event Name</h1>
-      <p className='font-semibold text-primary'>24 Dec 2024</p>
+      <h1 className='text-3xl mb-4'>{eventDetails.title}</h1>
+      <p className='font-semibold text-primary'>{formattedDate}</p>
       <div className='flex gap-2 my-4'>
         <svg
           className='h-6 fill-primary'
@@ -11,22 +33,9 @@ const EventDetails = () => {
         >
           <path d='M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z' />
         </svg>
-        <span>Sesame Street 10, 10101, Berlin</span>
+        <span>{eventDetails.location}</span>
       </div>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi ipsam
-        blanditiis hic commodi consequuntur quisquam, aperiam officia?
-        Molestiae, quia eos similique doloribus omnis distinctio, quod ipsum
-        praesentium officiis quo fugit inventore optio repellat quasi possimus
-        magni reiciendis magnam sed natus velit corrupti! Distinctio quos
-        architecto incidunt qui, accusamus recusandae eius, doloremque expedita
-        aliquam doloribus perspiciatis veniam! Ut similique, molestias officiis
-        consequatur hic magni consectetur. Quos, quibusdam praesentium! Qui
-        illum commodi ipsam sapiente dolore libero corrupti reiciendis voluptate
-        fugiat distinctio, ipsum sunt possimus ullam quam, aliquid enim. Nulla
-        numquam consequuntur pariatur ipsa possimus accusantium ducimus,
-        perferendis illo, sit accusamus laudantium assumenda.
-      </p>
+      <p>{eventDetails.description}</p>
     </main>
   );
 };
